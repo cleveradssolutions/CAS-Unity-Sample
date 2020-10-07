6d92bf2be71767f9453f83ad257c47200e8fdc0d
Eschenko_D <eschenkoden@gmail.com>
2020-10-07 17:19:00 +0300

    Init

---
+++ Assets/SampleController.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using CAS;
+
+public class SampleController : MonoBehaviour
+{
+    public ConsentStatus userConsent;
+    public CCPAStatus userCCPAStatus;
+    public Text versionText;
+    public Text bannerStatus;
+    public Text interstitialStatus;
+    public Text rewardedStatus;
+    public IMediationManager manager;
+
+    public void Start()
+    {
+        // -- Privacy Laws (Optional):
+        MobileAds.settings.userConsent = userConsent;
+        MobileAds.settings.userCCPAStatus = userCCPAStatus;
+
+        // -- Configuring CAS SDK (Optional):
+        MobileAds.settings.isExecuteEventsOnUnityThread = true;
+        MobileAds.settings.allowInterstitialAdsWhenVideoCostAreLower = true;
+
+        // -- Create manager:
+        manager = MobileAds.InitializeFromResources();
+        // OR same 
+        //manager = MobileAds.Initialize(managerID, AdFlags.Everything, true);
+
+        // -- Configuring Banner Ad
+        // Init Banner Size can be selected in Assets/CleverAdsSolutions/Settings
+        //manager.bannerSize = AdSize.AdaptiveBanner; 
+        manager.bannerPosition = AdPosition.BottomCenter;
+
+        // -- Subscribe to CAS events:
+        manager.OnRewardedAdCompleted += RewardedSuccessful;
+        // Any other callbacks from IMediationManager
+
+        // -- Get native CAS SDK version
+        versionText.text = MobileAds.GetSDKVersion();
+
+        InvokeRepeating( "OnRefreshStatus", 1.0f, 1.0f );
+    }
+
+    public void ShowBanner()
+    {
+        manager.ShowAd( AdType.Banner );
+    }
+
+    public void HideBanner()
+    {
+        manager.HideBanner();
+    }
+
+    public void SetBannerPosition(int positionEnum)
+    {
+        manager.bannerPosition = ( AdPosition )positionEnum;
+    }
+
+    public void SetBannerSize(int sizeID)
+    {
+        manager.bannerSize = ( AdSize )sizeID;
+    }
+
+    public void ShowInterstitial()
+    {
+        if(manager.IsReadyAd( AdType.Interstitial ))
+            manager.ShowAd( AdType.Interstitial );
+        else
+            Debug.LogError( "Interstitial Ad are not ready. Please try again later." );
+    }
+
+    public void ShowRewarded()
+    {
+        if (manager.IsReadyAd( AdType.Rewarded ))
+            manager.ShowAd( AdType.Rewarded );
+        else
+            Debug.LogError( "Rewarded Video Ad are not ready. Please try again later." );
+    }
+
+    private void OnRefreshStatus()
+    {
+        bannerStatus.text = manager.IsReadyAd( AdType.Banner) ? "Ready" : "Loading";
+        interstitialStatus.text = manager.IsReadyAd( AdType.Interstitial) ? "Ready" : "Loading";
+        rewardedStatus.text = manager.IsReadyAd( AdType.Rewarded) ? "Ready" : "Loading";
+    }
+
+    private void RewardedSuccessful()
+    {
+        Debug.Log("Rewarded Successful");
+    }
+}
---
+++ README.md
@@ -0,0 +1,23 @@
+# CleverAdsSolutions-Unity Sample
+The Clever Ads Solutions Unity plugin enables Unity developers to easily serve Mobile Ads on Android and iOS apps without having to write Java or Objective-C code. The plugin provides a C# interface for requesting ads that is used by C# scripts in your Unity project.
+
+## Documentation
+Check out our [documentation](https://github.com/cleveradssolutions/CAS-Unity#cleveradssolutions-unity-sdk-integration) on using the CAS SDK.
+
+## GitHub issue tracker
+To file bugs, make feature requests, or suggest improvements for the Unity Plugin SDK, please use [GitHub's issue tracker](https://github.com/cleveradssolutions/CAS-Unity/issues).
+
+## Support
+Site: [https://cleveradssolutions.com](https://cleveradssolutions.com)
+
+Technical support: Max  
+Skype: m.shevchenko_15  
+
+Network support: Vitaly  
+Skype: zanzavital  
+
+mailto:support@cleveradssolutions.com  
+
+## License
+The CAS iOS-SDK is available under a commercial license. See the LICENSE file for more info.
+
